26e4850a72e6cf337bc47824062418a3ee3c8152
Corey Perrigo <corey@perrigogames.com>
2017-02-04 02:26:53 -0800

    Java version bump to 7 remove ClickLambda, prefer no Gdx modification add Func classes add basic argument parsing

--- core/src/com/perrigogames/gdxjubeat/util/L.java
+++ core/src/com/perrigogames/gdxjubeat/util/L.java
@@ -9,31 +9,95 @@ public class L {
         if (l != null) l.invoke();
     }
 
-    public static <A> void $(Listener1 l, A arg1) {
+    public static <A> void $(Listener1<A> l, A arg1) {
         if (l != null) l.invoke(arg1);
     }
 
-    public static <A,B> void $(Listener2 l, A arg1, B arg2) {
+    public static <A,B> void $(Listener2<A,B> l, A arg1, B arg2) {
         if (l != null) l.invoke(arg1, arg2);
     }
 
-    public static <A,B,C> void $(Listener3 l, A arg1, B arg2, C arg3) {
+    public static <A,B,C> void $(Listener3<A,B,C> l, A arg1, B arg2, C arg3) {
         if (l != null) l.invoke(arg1, arg2, arg3);
     }
 
-    public interface Listener {
+    public static <A,B,C,D> void $(Listener4<A,B,C,D> l, A arg1, B arg2, C arg3, D arg4) {
+        if (l != null) l.invoke(arg1, arg2, arg3, arg4);
+    }
+
+    public static <A,B,C,D,E> void $(Listener5<A,B,C,D,E> l, A arg1, B arg2, C arg3, D arg4, E arg5) {
+        if (l != null) l.invoke(arg1, arg2, arg3, arg4, arg5);
+    }
+
+    interface Listener {
         void invoke();
     }
 
-    public interface Listener1<A> {
+    interface Listener1<A> {
         void invoke(A arg1);
     }
 
-    public interface Listener2<A,B> {
+    interface Listener2<A,B> {
         void invoke(A arg1, B arg2);
     }
 
-    public interface Listener3<A,B,C> {
+    interface Listener3<A,B,C> {
         void invoke(A arg1, B arg2, C arg3);
+    }
+
+    interface Listener4<A,B,C,D> {
+        void invoke(A arg1, B arg2, C arg3, D arg4);
+    }
+
+    interface Listener5<A,B,C,D,E> {
+        void invoke(A arg1, B arg2, C arg3, D arg4, E arg5);
+    }
+
+    public static <A> A $(Func<A> l) {
+        return (l != null) ? l.invoke() : null;
+    }
+
+    public static <A,B> A $(Func1<A,B> l, B arg1) {
+        return (l != null) ? l.invoke(arg1) : null;
+    }
+
+    public static <A,B,C> A $(Func2<A,B,C> l, B arg1, C arg2) {
+        return (l != null) ? l.invoke(arg1, arg2) : null;
+    }
+
+    public static <A,B,C,D> A $(Func3<A,B,C,D> l, B arg1, C arg2, D arg3) {
+        return (l != null) ? l.invoke(arg1, arg2, arg3) : null;
+    }
+
+    public static <A,B,C,D,E> A $(Func4<A,B,C,D,E> l, B arg1, C arg2, D arg3, E arg4) {
+        return (l != null) ? l.invoke(arg1, arg2, arg3, arg4) : null;
+    }
+
+    public static <A,B,C,D,E,F> A $(Func5<A,B,C,D,E,F> l, B arg1, C arg2, D arg3, E arg4, F arg5) {
+        return (l != null) ? l.invoke(arg1, arg2, arg3, arg4, arg5) : null;
+    }
+
+    interface Func<A> {
+        A invoke();
+    }
+
+    interface Func1<A,B> {
+        A invoke(B arg1);
+    }
+
+    interface Func2<A,B,C> {
+        A invoke(B arg1, C arg2);
+    }
+
+    interface Func3<A,B,C,D> {
+        A invoke(B arg1, C arg2, D arg3);
+    }
+
+    interface Func4<A,B,C,D,E> {
+        A invoke(B arg1, C arg2, D arg3, E arg4);
+    }
+
+    interface Func5<A,B,C,D,E,F> {
+        A invoke(B arg1, C arg2, D arg3, E arg4, F arg5);
     }
 }
--- desktop/src/com/perrigogames/gdxjubeat/desktop/DesktopLauncher.java
+++ desktop/src/com/perrigogames/gdxjubeat/desktop/DesktopLauncher.java
@@ -3,12 +3,42 @@ package com.perrigogames.gdxjubeat.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.perrigogames.gdxjubeat.GdxJubeat;
+import com.perrigogames.gdxjubeat.util.Log;
 
+/** Desktop launcher class for gdxjubeat
+ *
+ * Usage:  java gdxjubeat [-w]
+ *
+ * -w: run the game in windowed mode */
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = GdxJubeat.VIEWPORT_WIDTH;
-		config.height = GdxJubeat.VIEWPORT_HEIGHT;
-		new LwjglApplication(new GdxJubeat(), config);
+
+	public static void main (String[] args) {
+		new DesktopLauncher().parseArgs(args).start();
 	}
+
+	boolean fullscreen = true;
+
+	DesktopLauncher start() {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = GdxJubeat.VIEWPORT_WIDTH;
+        config.height = GdxJubeat.VIEWPORT_HEIGHT;
+        config.fullscreen = fullscreen;
+        new LwjglApplication(new GdxJubeat(), config);
+        return this;
+    }
+
+    DesktopLauncher parseArgs(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            switch(args[i]) {
+                case "-w":
+                case "-W":
+                    fullscreen = false;
+                    break;
+                default:
+                    Log.v("Argument \"%s\" is unsupported", args[i]);
+                    break;
+            }
+        }
+        return this;
+    }
 }
